0738efca0978c420910ad41bfa3fa45553ee6971
Alexmerser54 <alexmerser541@gmail.com>
2023-01-13 00:30:51 +0700

    Laba 6 and 7

---
+++ Laba7_16.4b/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7_16._4b
+{
+
+    struct Date
+    {
+        int day;
+        int month;
+        int year;
+
+
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public int Day
+        {
+            get
+            {
+                return day;
+            }
+        }
+
+        public int Month
+        {
+            get
+            {
+                return month;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{day}.{month}.{year}";
+        }
+
+        public static int DaysInMonth(int month)
+        {
+            if (month == 1 | month == 3 | month == 5 | month == 7 | month == 8 | month == 10 | month == 12)
+                return 31;
+            else if (month == 4 | month == 6 | month == 9 | month == 11)
+                return 30;
+            return 28;
+        }
+
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date today;
+            Console.WriteLine("Введите дату");
+            string[] input = Console.ReadLine().Split('.');
+
+            today = new Date(Convert.ToInt32(input[0]), Convert.ToInt32(input[1]), Convert.ToInt32(input[2]));
+
+            int day;
+            int month;
+            int year;
+
+            if (today.Day == 1)
+            {
+                if (today.Month == 1)
+                {
+                    month = 12;
+                    year = today.Year - 1;
+                }
+                else
+                {
+                    month = today.Month - 1;
+                    year = today.Year;
+                }
+                day = Date.DaysInMonth(month);
+            }
+            else
+            {
+                day = today.Day - 1;
+                month = today.Month;
+                year = today.Year;
+            }
+
+            Date yesterday = new Date(day, month, year);
+            Console.WriteLine("Вчерашняя дата: " + yesterday);
+        }
+    }
+}
---
+++ Laba7_18.12/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7_18._12
+{
+
+    struct Note
+    {
+        string name;
+        string lastname;
+        string phone;
+        int[] birthDate;
+
+        public Note(string name, string lastname, string phone, int[] birthDate)
+        {
+            this.name = name;
+            this.lastname = lastname;
+            this.phone = phone;
+            this.birthDate = birthDate;
+        }
+
+        public string Name => name;
+        public string Lastname => lastname;
+        public string Phone => phone;
+        public int[] BirthDate => birthDate;
+
+        public override string ToString()
+        {
+            return $"{Name} {Lastname}, {Phone}, {BirthDate[0]}.{BirthDate[1]}.{BirthDate[2]}";
+        }
+
+    }
+
+    class Program
+    {
+
+
+        public static Note ParseStringToNote(string str)
+        {
+            string[] tmp = str.Split(new string[] { ", " }, StringSplitOptions.None);
+
+            int[] date = new int[3];
+
+            string[] name = tmp[0].Split(' ');
+
+
+            string[] dateTmp = tmp[2].Split('.');
+
+            for (int j = 0; j < date.Length; j++)
+            {
+                date[j] = Convert.ToInt32(dateTmp[j]);
+            }
+
+            return new Note(name[0], name[1], tmp[1], date);
+        }
+
+        public static Note[] FileToNotes(string fileName, int count)
+        {
+            Note[] people = new Note[count];
+
+            string[] lines = File.ReadAllLines(fileName);
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = ParseStringToNote(lines[i]);
+
+            }
+            return people;
+        }
+
+
+
+        public static void WriteNotes(Note[] people, string filename)
+        {
+            foreach (var item in people)
+            {
+                File.AppendAllText(filename, item.ToString() + Environment.NewLine);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Note[] people = new Note[8];
+            string input;
+
+            Console.WriteLine("Выберете действие:");
+            Console.WriteLine("1) Записать 8 человек в файл");
+            Console.WriteLine("2) Считать данные из файла");
+            Console.WriteLine("3) Поиск человека по номеру");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+
+                    File.WriteAllText("note.txt", "");
+
+                    for (int i = 0; i < people.Length; i++)
+                    {
+                    A: try
+                        {
+                            input = Console.ReadLine();
+                            people[i] = ParseStringToNote(input);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Данные введены в неверном формате. Введите данные в верном формате");
+                            goto A;
+                        }
+
+                    }
+
+                    for (int i = 0; i < people.Length; i++)
+                    {
+                        for (int j = i + 1; j < people.Length; j++)
+                        {
+                            if (people[i].BirthDate[2] > people[j].BirthDate[2] ||
+                                (people[i].BirthDate[2] > people[j].BirthDate[2] && people[i].BirthDate[1] > people[j].BirthDate[1]) ||
+                                (people[i].BirthDate[2] > people[j].BirthDate[2] && people[i].BirthDate[1] > people[j].BirthDate[1] && people[i].BirthDate[0] > people[j].BirthDate[0]))
+                            {
+                                Note tmp = people[i];
+                                people[i] = people[j];
+                                people[j] = tmp;
+                            }
+                        }
+                    }
+
+                    WriteNotes(people, "note.txt");
+
+
+                    break;
+                case 2:
+
+                    foreach (var item in File.ReadAllLines("note.txt"))
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    break;
+                case 3:
+
+                    string number = Console.ReadLine();
+
+                    string[] lines = File.ReadAllLines("note.txt");
+
+                    for (int i = 0; i < people.Length; i++)
+                    {
+                        people[i] = ParseStringToNote(lines[i]);
+                    }
+
+                    bool flag = false;
+
+                    foreach (var item in people)
+                    {
+                        if (item.Phone == number)
+                        {
+                            Console.WriteLine(item.ToString());
+                            flag = true;
+                        }
+                    }
+
+                    if (flag == false) Console.WriteLine("Нет людей с таким номером");
+
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
